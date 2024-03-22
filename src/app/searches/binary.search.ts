786c267a5d74fa0fe476b5b2019ec2218ae6c594
Johnny Cowboy <johnnycowboy3033@gmail.com>
2024-03-21 20:57:41 -0400

    commit

--- src/app/searches/binary.search.ts
+++ src/app/searches/binary.search.ts
@@ -1,39 +1,50 @@
 export class BinarySearch {
 
-  private _pointerHeader:number = 0;
-  private _pointerMiddle:number = 0;
-  private _pointerTail:number = 0;
+  //POINTERS
+  private _head:number = 0;
+  private _middle:number = 0;
+  private _tail:number = 0;
 
   constructor() {
   }
 
-
+  calculateMiddle(head:number,tail:number,){
+    return head + Math.floor(( tail - head) / 2);
+  }
 
   pointer(stepCount:number,element:number[],target:number):number[] {
 
       if(stepCount === 0){
-        this._pointerHeader = 0;
-        this._pointerTail =  element.length - 1;
+        this._head = 0;
+        this._tail =  element.length - 1;
+        this._middle = this.calculateMiddle(this._head,this._tail);
+
+        return [this._head, this._middle, this._tail];
       }
 
-      this._pointerMiddle = this._pointerHeader + Math.floor(( this._pointerTail - this._pointerHeader) / 2);
-
-
       //Check if the target is found if the element is in the array
-      if (element[this._pointerMiddle] === target){
-        return [this._pointerMiddle];
+      if (element[this._middle] === target){
+        return [this._middle];
       }else{
-        return [this._pointerHeader, this._pointerMiddle, this._pointerTail];
+
+        if(this._middle > target){
+          this._head = this._middle + 1;
+        }else{
+          this._tail = this._middle - 1;
+        }
+
+        this._middle = this.calculateMiddle(this._head,this._tail);
+
+        return [this._head, this._middle, this._tail];
       }
   }
 
+  get head(): number {return this._head;}
+  set head(value: number) {this._head = value;}
 
-  get pointerHeader(): number {return this._pointerHeader;}
-  set pointerHeader(value: number) {this._pointerHeader = value;}
+  get middle(): number {return this._middle;}
+  set middle(value: number) {this._middle = value;}
 
-  get pointerMiddle(): number {return this._pointerMiddle;}
-  set pointerMiddle(value: number) {this._pointerMiddle = value;}
-
-  get pointerTail(): number {return this._pointerTail;}
-  set pointerTail(value: number) {this._pointerTail = value;}
+  get tail(): number {return this._tail;}
+  set tail(value: number) {this._tail = value;}
 }
